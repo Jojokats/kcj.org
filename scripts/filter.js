5ff3b48690a714c41d0b7545d5eba37524e4a54b
kimhyonh <pengkim.professional@gmail.com>
2017-07-17 16:58:33 -0400

    add nothing found when there's no result

--- scripts/filter.js
+++ scripts/filter.js
@@ -8,6 +8,8 @@ function init(){
   g.province = document.getElementById('province');
   g.type = document.getElementById('type');
   g.month = document.getElementById('month');
+  g.notFound = document.getElementById('notFound');
+  hide(g.notFound, true);
 
   g.filterBtn = document.getElementById('filterBtn');
   g.filterBtn.addEventListener('click', filter);
@@ -23,19 +25,26 @@ function filter(){
 
   var listItems = g.list.getElementsByClassName('list-item');
   for(var i=0; i<listItems.length; i++){
-    var type = listItems[i].getElementsByClassName('type')[0].innerHTML.toLowerCase().trim();
-    var location = listItems[i].getElementsByClassName('location')[0].firstElementChild;
-    var date = listItems[i].getElementsByClassName('date')[0].firstElementChild;
-    if(location){
-      location = location.className.toLowerCase().trim();
+    var type = listItems[i].getElementsByClassName('type')[0];
+    var location = listItems[i].getElementsByClassName('location')[0];
+    var date = listItems[i].getElementsByClassName('date')[0];
+    if(type){
+      type = type.innerHTML.toLowerCase().trim();
+    } else {
+      type = '';
+    }
+    if(location && location.firstElementChild){
+      location = location.firstElementChild.className.toLowerCase().trim();
     } else {
       location = '';
     }
-    if(date){
-      date = date.className.toLowerCase().trim();
+    if(date && date.firstElementChild){
+      date = date.firstElementChild.className.toLowerCase().trim();
     } else {
       date = '';
     }
+
+  //  console.log(location);
 
     if(type.indexOf(typeSelected) > -1  && location.indexOf(provinceSelected) > -1
         && date.indexOf(monthSelected) > -1){
@@ -44,8 +53,23 @@ function filter(){
       hide(listItems[i], true);
     }
   }
+
+  noItemFond(listItems);
 }
 
+function noItemFond(listItems){
+
+  var isDisplay = false;
+  for(var i=0; i<listItems.length; i++){
+    if(listItems[i].style.display === ''){
+      isDisplay = true;
+    }
+  }
+
+  if(isDisplay == false){
+    hide(g.notFound, false);
+  }
+}
 
 function hide(element, hide){
   if(hide){
