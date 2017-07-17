eb02b8646f835acabc8b3e1d34c6578a87a7104e
kimhyonh <pengkim.professional@gmail.com>
2017-07-17 16:25:34 -0400

    add all to the select list in the workshop page

--- scripts/filter.js
+++ scripts/filter.js
@@ -12,60 +12,45 @@ function init(){
   g.filterBtn = document.getElementById('filterBtn');
   g.filterBtn.addEventListener('click', filter);
 }
-/*
-var g = {};
 
-$(document).ready(function(){
-    g.province = document.getElementById('province');
-    g.type = document.getElementById('type');
-    g.month = document.getElementById('month');
-
-    g.filterBtn = $('#filterBtn');
-    g.filterBtn.click(filter);
-
-    //g.filterBtn = document.getElementById('filterBtn');
-  }
-);
-*/
+/**
+ * Filter the base on the user selected on the workshop page
+ */
 function filter(){
-  var provinceSelected = g.province.options[g.province.selectedIndex].value.toLowerCase();
-  var typeSelected = g.type.options[g.type.selectedIndex].value.toLowerCase();
-  var monthSelected = g.month.options[g.month.selectedIndex].value.toLowerCase();
+  var provinceSelected = g.province.options[g.province.selectedIndex].value.toLowerCase().trim();
+  var typeSelected = g.type.options[g.type.selectedIndex].value.toLowerCase().trim();
+  var monthSelected = g.month.options[g.month.selectedIndex].value.toLowerCase().trim();
 
   var listItems = g.list.getElementsByClassName('list-item');
-  console.log(listItems.length);
   for(var i=0; i<listItems.length; i++){
-    var type = listItems[i].getElementsByClassName('type')[0].innerHTML.toLowerCase();
+    var type = listItems[i].getElementsByClassName('type')[0].innerHTML.toLowerCase().trim();
     var location = listItems[i].getElementsByClassName('location')[0].firstElementChild;
     var date = listItems[i].getElementsByClassName('date')[0].firstElementChild;
-  //  console.log(location.firstElementChild.className);
     if(location){
-      location = location.className.toLowerCase();
+      location = location.className.toLowerCase().trim();
     } else {
       location = '';
     }
     if(date){
-      date = date.className.toLowerCase();
+      date = date.className.toLowerCase().trim();
     } else {
       date = '';
     }
-    if(type.trim().indexOf(typeSelected.trim()) > -1
-        && location.trim().indexOf(provinceSelected.trim()) > -1
-        && date.trim().indexOf(monthSelected.trim()) > -1){
-      listItems[i].style.display = '';
-    } else {
-      listItems[i].style.display = 'none';
-    }
 
+    if(type.indexOf(typeSelected) > -1  && location.indexOf(provinceSelected) > -1
+        && date.indexOf(monthSelected) > -1){
+      hide(listItems[i], false);
+    } else {
+      hide(listItems[i], true);
+    }
   }
 }
 
 
-function sortByProvinces(btn){
-
-  var provinceOptions = {
-    valueNames: [ 'AB', 'BC', 'ON', 'QC' ]
-  };
-
-  var userList = new List('filter', provinceOptions);
+function hide(element, hide){
+  if(hide){
+    element.style.display = 'none';
+  } else {
+    element.style.display = '';
+  }
 }
