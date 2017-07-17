e1e9e0ecfcd7097554607695eb4108e17914b95a
thatNinja42 <josepepin2@gmail.com>
2017-07-17 16:32:22 -0700

    faq page

---
+++ scripts/faq.js
@@ -0,0 +1,25 @@
+  var g = {};
+$(document).ready(function() {
+  init();
+});
+
+function init() {
+  g.titles = document.getElementsByClassName("panel-title");
+
+
+  $(g.titles).click(function(e) {
+    var target = e.target || e.srcElement;
+    target = target.parentElement.parentElement;
+
+    if(target.className == "panel-title glyphicon glyphicon-triangle-bottom") {
+      for (var i = 0; i < g.titles.length; i++) {
+        g.titles[i].className = "panel-title glyphicon glyphicon-triangle-bottom";
+      }
+      target.className = "panel-title glyphicon glyphicon-triangle-top";
+    } else if (target.className == "panel-title glyphicon glyphicon-triangle-top") {
+      target.className = "panel-title glyphicon glyphicon-triangle-bottom";
+    }
+  });
+
+
+}
