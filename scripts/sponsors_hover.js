733a6bff60c34283d2535ee31dfce3f80050f475
kimhyonh <pengkim.professional@gmail.com>
2017-08-02 10:39:06 -0400

    Finished hover sponsor images

--- scripts/sponsors_hover.js
+++ scripts/sponsors_hover.js
@@ -1,15 +1,25 @@
 $(function(){
-  $('#partnersSponsors a').hover(function(){
-  //  var orginalSrc = this.firstElementChild.src;
-    var src = this.firstElementChild.src.split('/');
-    var sponsor = src[src.length - 1];
-    sponsor = sponsor.substr(0, sponsor.indexOf('-'));
-
-/*
-    for(var i=0; i<src.length - 2; i++){
-      src = src[i] + '';
-    }*/
-    console.log(src);
-    //this.firstElementChild.src = src.join('');
+  $('#partnersSponsors img').hover(function(){
+    hover(this, true);
+  });
+  $('#partnersSponsors img').mouseout(function(){
+    hover(this, false);
   });
 });
+
+function hover(img, hovered){
+  var src = img.src.split('/');
+  var sponsor = src[src.length - 1];
+  sponsor = sponsor.substr(0, sponsor.indexOf('-'));
+
+  var path = '';
+  for(var i=0; i<src.length - 1; i++){
+    path += src[i] + '/';
+  }
+
+  if(hovered){
+    img.src = path + sponsor + '-Colour.png';
+  } else {
+    img.src = path + sponsor + '-Grey.png';
+  }
+}
