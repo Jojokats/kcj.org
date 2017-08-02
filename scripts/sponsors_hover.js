060364c9f6eec85a4283466d1b0ef59ea1834994
kimhyonh <pengkim.professional@gmail.com>
2017-08-02 10:42:49 -0400

    fixed the codeing structure

--- scripts/sponsors_hover.js
+++ scripts/sponsors_hover.js
@@ -1,23 +1,23 @@
-$(function(){
-  $('#partnersSponsors img').hover(function(){
+$(function() {
+  $('#partnersSponsors img').hover(function() {
     hover(this, true);
   });
-  $('#partnersSponsors img').mouseout(function(){
+  $('#partnersSponsors img').mouseout(function() {
     hover(this, false);
   });
 });
 
-function hover(img, hovered){
+function hover(img, hovered) {
   var src = img.src.split('/');
   var sponsor = src[src.length - 1];
   sponsor = sponsor.substr(0, sponsor.indexOf('-'));
 
   var path = '';
-  for(var i=0; i<src.length - 1; i++){
+  for (var i = 0; i < src.length - 1; i++) {
     path += src[i] + '/';
   }
 
-  if(hovered){
+  if (hovered) {
     img.src = path + sponsor + '-Colour.png';
   } else {
     img.src = path + sponsor + '-Grey.png';
