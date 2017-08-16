a15e4232fff8d653ce71080b67dff256d7bbe068
thatNinja42 <josepepin2@gmail.com>
2017-08-16 14:32:33 -0700

    email fixes

--- scripts/email_saver.js
+++ scripts/email_saver.js
@@ -1,14 +1,20 @@
 var g = {};
 $(document).ready(function() {
   g.btn = document.getElementsByClassName("submit_email");
-  g.lang = document.getElementById("language").getElementsByTagName("li");
+  g.lang = document.getElementById("language").getElementsByClassName("active")[0].children[0].innerHTML;
 $(g.btn).click(function(e) {
+  console.log("1")
   e.preventDefault();
   g.email = this.parentElement.children[0]
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   if($(g.email).val() !== '' && regex.test($(g.email).val())){
     $.cookie("newslleter_email", g.email.value);
+    if(g.lang == "EN") {
     window.location.href = "contact.html#mc_embed_signup_scroll";
+  } else if (g.lang == "FR") {
+    console.log("FR");
+    window.location.href = "nous_joindre.html#mc_embed_signup_scroll";
+    }
   }
 });
 });
