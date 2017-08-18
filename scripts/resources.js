5518db29f652192b339b4584a272a43e222cc274
thatNinja42 <josepepin2@gmail.com>
2017-08-18 16:54:07 -0700

    resources nav bar

---
+++ scripts/resources.js
@@ -0,0 +1,19 @@
+var g = {};
+$(document).ready(function() {
+  g.scratch = $("#scratch")[0];
+  g.html = $("#html")[0];
+  g.python = $("#python")[0];
+  g.open = document.getElementById("open");
+  g.nav = document.getElementById("mySidenav");
+  g.close = $("#close")[0];
+
+  $(g.scratch).on( "click", function() {
+    g.nav.style.width = "18%";
+  });
+  $(g.close).on( "click", function() {
+    console.log("close");
+    g.nav.style.width = "0px";
+  });
+
+
+});
