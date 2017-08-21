2ad57ab81e67d5b00ee12bd626c747c1b520595b
thatNinja42 <josepepin2@gmail.com>
2017-08-21 12:30:35 -0700

    changes

--- scripts/resources.js
+++ scripts/resources.js
@@ -1,19 +1,66 @@
 var g = {};
 $(document).ready(function() {
-  g.scratch = $("#scratch")[0];
-  g.html = $("#html")[0];
-  g.python = $("#python")[0];
-  g.open = document.getElementById("open");
-  g.nav = document.getElementById("mySidenav");
-  g.close = $("#close")[0];
 
-  $(g.open).on( "click", function() {
-    g.nav.style.width = "18%";
-  });
-  $(g.close).on( "click", function() {
-    console.log("close");
-    g.nav.style.width = "0px";
-  });
+  g.in_class = $("#in-class").find("img");
+  g.workshop = ($("#workshop").find("img"));
+  g.teacher_training = ($("#teacher_training").find("img"));
+  g.nav = $("#nav_options")[0];
+  g.main = $("#main_menue")[0];
+  g.back = $("#back")[0];
+  g.pdfs = $("#pdfs")[0];
+  g.pdf1 = $("#batch_pdf1")[0];
+  g.pdf2 = $("#batch_pdf2")[0];
+  g.pdf3 = $("#batch_pdf3")[0];
 
+  $(g.in_class).on( "click", toggle_options);
+  $(g.workshop).on( "click", toggle_options);
+  $(g.teacher_training).on( "click", toggle_options);
 
 });
+
+function toggle_options(e) {
+  g.e = e;
+  g.target = e.target.id
+  //g.nav[0].children[0]
+  switch (g.target) {
+    case "scratch":
+        show_pdfs(e, this, 0, 0);
+        break;
+    case "html":
+        show_pdfs(e, this, 0, 1);
+        break;
+    case "python":
+        show_pdfs(e, this, 0, 2);
+        break;
+    case "1_hour_workshop":
+        show_pdfs(e, this, 1, 0);
+        break;
+    case "2_hour_workshop":
+        show_pdfs(e, this, 1, 1);
+        break;
+    case "computational_thinking":
+        show_pdfs(e, this, 2, 0);
+        break;
+    case "computer_programming":
+        show_pdfs(e, this, 2, 1);
+        break;
+
+      }
+ $(g.back).on( "click", function() {
+   $(g.main).removeClass('hidden');
+   $(g.nav).addClass('hidden');
+   $(g.current).addClass('hidden');
+  });
+}
+function show_pdfs(e, current, section, option) {
+  g.current = g.pdfs.children[section].children[option];
+  $(g.main).addClass('hidden');
+  $(g.nav).removeClass('hidden');
+  $(g.current).removeClass('hidden');
+  g.options = g.nav.children[section].children[1].getElementsByTagName('span');
+  $(g.options).on( "click", toggle_nav_options);
+}
+function toggle_nav_options(e) {
+
+
+}
