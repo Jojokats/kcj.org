1c6f38ddba90ad46b75873186a644eef12fdc242
kimhyonh <pengkim.professional@gmail.com>
2017-08-21 14:40:01 -0400

    fixed the home page

--- scripts/design.js
+++ scripts/design.js
@@ -1,35 +1,7 @@
 $(document).ready(function() {
   "use strict";
-  /**
-   * Set the height of the bg-empty class resposive the size of the image_h
-   * in the carousel.
-   */
-  $(".bg-empty").height($("#video video").height() + 90);
-  var size = $("#myCarousel img").length;
-  for (var i = 0; i < size; i++) {
-    $("#myCarousel img").eq(i).load(function() {
-      var image_h = this.height;
-      $(".bg-empty").height(image_h + 100);
-    });
-  }
-  $(window).on('resize', function() {
-    $("#video").trigger('load');
-  }).trigger('resize');
 
-  /**
-   * When the user scroll top/down, the opacity of the black background-color
-   * on the class bg-empty resposive to the scroll. For example, if the user
-   * scroll down, the background-color is getting darker and vise-versa.
-   */
-  // $(window).scroll(function() {
-  //   var scrollTop = $(this).scrollTop();
-  //   var opacity = scrollTop / (scrollTop + 90);
-  //   var carousel_top = $("#video").css("top");
-  //   var top = parseInt(carousel_top.substr(0, carousel_top.indexOf("p")));
-  //   $(".bg-empty").each(function() {
-  //     if (opacity < 1) {
-  //       $(this).css("opacity", opacity);
-  //     }
-  //   });
-  // }).scroll();
+  if($(document).width() < 450){
+    $('#newsLetter h2').replaceWith('<h3 class="text-uppercase text-white">Never miss a thing! <strong>Sign up</strong> for our newsletter</h3>');;
+  }
 });
