9db0e1a4f6fefecd6fa00d65f67759fa8f373b4c
kimhyonh <pengkim.professional@gmail.com>
2017-08-22 17:35:31 -0400

    added support pages

--- scripts/design.js
+++ scripts/design.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   "use strict";
 
   if($(document).width() < 450){
-    $('#newsLetter h2').replaceWith('<h3 class="text-uppercase text-white">Never miss a thing! <strong>Sign up</strong> for our newsletter</h3>');;
+    $('#newsLetter h2').replaceWith('<h3 class="text-uppercase text-darkblue">Never miss a thing! <strong>Sign up</strong> for our newsletter</h3>');;
   }
 });
