ccfc88fe498d3e84eb635c1b3f7f7c053c75501f
thatNinja42 <josepepin2@gmail.com>
2017-08-24 16:07:00 -0700

    changes

--- scripts/resources.js
+++ scripts/resources.js
@@ -1,16 +1,23 @@
 var g = {};
 $(document).ready(function() {
-
+  /*
+  * the following 3 variables will contain all the
+  * images that will be clickble on the resources page.
+  */
   g.in_class = $("#in-class").find("img");
   g.workshop = ($("#workshop").find("img"));
   g.teacher_training = ($("#teacher_training").find("img"));
-  g.main = $("#main_menue")[0];
-  g.back = $(".back");
-  g.pdfs = $("#pdfs")[0];
-  g.pdf1 = $("#batch_pdf1")[0];
-  g.pdf2 = $("#batch_pdf2")[0];
-  g.pdf3 = $("#batch_pdf3")[0];
 
+  g.main = $("#main_menue")[0]; //this is the original menu the user first sees when resources is loaded
+  g.back = $(".back"); //contains all the back buttons on the page.
+  g.pdfs = $("#pdfs")[0];//contains the full section with all the sub menues of the each catogory
+  g.pdf1 = $("#batch_pdf1")[0]; //first section of pdfs
+  g.pdf2 = $("#batch_pdf2")[0]; //second section of pdfs
+  g.pdf3 = $("#batch_pdf3")[0]; //three section of pdfs
+
+/*
+* the following three event listners are for when the user clicks on one of the images.
+*/
   $(g.in_class).on( "click", toggle_options);
   $(g.workshop).on( "click", toggle_options);
   $(g.teacher_training).on( "click", toggle_options);
