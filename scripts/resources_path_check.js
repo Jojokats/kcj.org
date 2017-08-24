acf4c2ef789bbc1def23787687e0b741b688ae17
thatNinja42 <josepepin2@gmail.com>
2017-08-23 17:30:48 -0700

    changes

--- scripts/resources_path_check.js
+++ scripts/resources_path_check.js
@@ -2,5 +2,16 @@ var g = {};
 $(document).ready(function() {
   console.log(g.lang + " <====");
   g.links = $('.resources_link');
+  g.mac = "mac";
+
+ if (window.navigator.platform.substring(0,3) == "Mac") {
+      set_path(g.mac);
+  }
 
 });
+
+function set_path (path) {
+  for (var i = 0; i < g.links.length; i++) {
+    g.links[i].href.replace("windows", "mac");
+  }
+}
