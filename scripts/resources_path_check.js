131f7e1e4089d6e050fa5de9c19922de75b8bec3
thatNinja42 <josepepin2@gmail.com>
2017-08-24 13:05:37 -0700

    changes

--- scripts/resources_path_check.js
+++ scripts/resources_path_check.js
@@ -2,14 +2,22 @@ var g = {};
 $(document).ready(function() {
   g.links = document.getElementsByClassName('resources_link');
 
- if (window.navigator.platform.substring(0,3) === "Mac") {
+ if (window.navigator.platform.substring(0,3) === "Win") {
       set_path();
   }
 });
-
 function set_path() {
+  g.temp = [];
   for (var i = 0; i < g.links.length; i++) {
-    g.links[i].href.replace("windows", "mac");
+    g.temp[i] = g.links[i].href;
+    g.temp[i].replace("windows", "hiii");
   }
-  console.log(g.links[0].href + " <=======");
+  for (var i = 0; i < g.links.length; i++) {
+  //  g.links[i].href = "";
+    $(g.links[i]).removeAttr("href");
+    console.log(g.links[i].href);
+  }
+  for (var i = 0; i < g.links.length; i++) {
+    g.links[i].href = g.temp[i]
+  }
 }
