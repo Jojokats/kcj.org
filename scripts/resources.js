2798b2bdc2bc72591ea1f369e4ebd4b1f0d9ed7a
thatNinja42 <josepepin2@gmail.com>
2017-08-30 15:27:07 -0700

    2 hour section on the resources fixed

--- scripts/resources.js
+++ scripts/resources.js
@@ -23,48 +23,53 @@ $(document).ready(function() {
   $(g.teacher_training).on( "click", toggle_options);
 
 });
-
+/**
+* Toggle through the different sections, the switch statement,
+* will check the id of the target click and will send the even
+* the this which is the target the rose the event as well of two
+* integers that represent there posstion on the original menue.
+* For example 0,0 represents inclassroom section and points to the
+* intro div of said section.
+*/
 function toggle_options(e) {
   $(window).scrollTop(0);
   g.target = e.target.id
   switch (g.target) {
     case "intro":
-        show_pdfs(e, this, 0, 0);
+        show_pdfs(e, this, 0, 0);//intro
         break;
     case "scratch":
-        show_pdfs(e, this, 0, 1);
+        show_pdfs(e, this, 0, 1);//scratch
         break;
     case "html":
-        show_pdfs(e, this, 0, 2);
+        show_pdfs(e, this, 0, 2);//html
         break;
     case "python":
-        show_pdfs(e, this, 0, 3);
+        show_pdfs(e, this, 0, 3);//python
         break;
     case "unplugged":
-        show_pdfs(e, this, 0, 4);
+        show_pdfs(e, this, 0, 4);//unplugged
         break;
     case "activiies":
-        show_pdfs(e, this, 0, 5);
+        show_pdfs(e, this, 0, 5);//activity
         break;
 
-    case "1_hour_workshop":
-        show_pdfs(e, this, 1, 0);
-        break;
     case "2_hour_workshop":
-        show_pdfs(e, this, 1, 1);
+        show_pdfs(e, this, 1, 0);//2-hour
         break;
 
     case "computational_thinking":
-        show_pdfs(e, this, 2, 0);
+        show_pdfs(e, this, 2, 0);//Computational Thinking
         break;
     case "computer_programming":
-        show_pdfs(e, this, 2, 1);
+        show_pdfs(e, this, 2, 1);//Computer Programming
         break;
     case "internet_web":
-        show_pdfs(e, this, 2, 2);
+        show_pdfs(e, this, 2, 2);//Internet Web
         break;
 
       }
+  //the click event handler for the back button
    $(g.back).on( "click", function() {
      $(window).scrollTop(0);
      $(g.main).removeClass('hidden');
@@ -72,6 +77,9 @@ function toggle_options(e) {
      $(g.current).addClass('hidden');
     });
 }
+/**
+* Shows the section of the given section indexes
+*/
 function show_pdfs(e, current, section, option) {
   g.current = g.pdfs.children[section].children[option];
   g.section = section;
