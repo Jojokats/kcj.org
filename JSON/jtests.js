cd1fdd00df023ce2625fe09a5acf9ad3066d5765
werner castanaza <wernercastanaza@gmail.com>
2017-10-10 16:27:44 -0700

    all the set up for displying the docs for each block is made, the making of the HTML elements is left and for that the pseudo code is layed out

--- JSON/jtests.js
+++ JSON/jtests.js
@@ -5,13 +5,29 @@ $(function(){
   g.count = 0;
   setUpConnection('resources.json');
   setUpConnection('inclass.json');
+  setContent();
 });
 function showPdfs(e) {
-  g.e = e;
-//  $(g.body).addClass("hidden");
-//  g.e = g.e.target.src.substr(g.e.target.src.lastIndexOf('/')+1);
-  if(g.response.section1.title) {
-
+  var a = this.id
+  for (var key in g.inclass) {
+    if(this.id == g.inclass[key].title) {
+      $(g.main_options).addClass("hidden");
+      setDocs(g.inclass[key].title, g.inclass[key].documents);
+      break;
+    }
+  }
+}
+function setDocs(header, arrayOfDocs) {
+  var title = $("<h1></h>").addClass(
+    'text-orange text-uppercase text-center padding-bot-small').text(header);
+    $(g.body).append(title);
+var section = $("<div></div>").addClass('container-fluid border-top row-centered padding-bot-medium');
+  for (var key in arrayOfDocs) {
+    g.a = arrayOfDocs[key];
+     title = $("<h3></h3>").addClass(
+       'text-orange text-uppercase text-center padding-bot-small').text(arrayOfDocs[key].title);
+       $(section).append(title);
+       $(g.body).append(section);
   }
 }
 function setUpConnection(file) {
@@ -23,7 +39,6 @@ function setUpConnection(file) {
 function chkResponse(){
 	if (g.file.readyState === 4 && g.file.status === 200) {
     setVariables(JSON.parse(g.file.responseText));
-    setContent();
 	}
 }
 function setVariables(response) {
@@ -47,17 +62,24 @@ function setContent() {
       }
     }//end of if
   }// end of for each
+  //all image blocks have been made now I need to place an id
+  //in order to uniquly identify each one.
+  g.imgBlocks = $(".img");
+  var i = 0;
+  for (var key in g.inclass) {
+      $(g.imgBlocks[i]).attr("id", g.inclass[key].title);
+      i++;
+  }
 }
-function placeSubSections(key, title) {
+function placeSubSections(key, title, index) {
   var subsection = g.response[key].subsection;
-  var sucContainer = $("<div></div>").addClass('row row-centered');
+  var subContainer = $("<div></div>").addClass('row row-centered');
   for (var i = 0; i < g.response[key].subsection.length; i++) {
     var subTitle = $("<h4></h4>").addClass('text-blue text-center padding-bot-small padding-top-small').text(g.response[key].subsection[i])
-    $(sucContainer).append(subTitle);
-
-    placeBlocks(g.response[key].block[g.response[key].subsection[i]], sucContainer);
+    $(subContainer).append(subTitle);
+    placeBlocks(g.response[key].block[g.response[key].subsection[i]], subContainer);
   }
-  $(title).append(sucContainer);
+  $(title).append(subContainer);
 }
 function placeBlocks(block, container) {
   for (var i = 0; i < block.length; i++) {
