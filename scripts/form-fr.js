3f3dbdaafb732498116ef5c4dd02608076771ee8
kimhyonh <pengkim.professional@gmail.com>
2017-10-30 16:26:16 -0400

    change the form

--- scripts/form-fr.js
+++ scripts/form-fr.js
@@ -1,5 +1,5 @@
 window.FF_OnAfterSave = function() {
-    $.cookie('formCompleted', true, { expire: 365, path: '/'});
+    $.cookie('formCompleted', true);
     location = 'ressources.html';  
 }
 
--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,4 @@
 window.FF_OnAfterSave = function() {
-    $.cookie('formCompleted', true, { expire: 365, path: '/'});
+    $.cookie('formCompleted', true);
     location = 'resources.html';  
 }
