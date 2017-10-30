40d20ec3fef88ce148a95426686fbe74418c4629
kimhyonh <pengkim.professional@gmail.com>
2017-10-30 12:35:39 -0400

    remove the OK button from the form

--- scripts/form.js
+++ scripts/form.js
@@ -1,21 +1,4 @@
-$(function(){
-  var interval = setInterval(function(){
-    var btnok = document.getElementsByClassName('ff-btn-submit')[0];
-    console.log(btnok);
-    if(btnok){
-      clearInterval(interval);
-      btnOkHandler(btnok);
-    }
-  }, 1000);
-});
-
-function btnOkHandler(btn){
-  btn.addEventListener('click', function(){
-    console.log("btn clicked");
-    // window.SubmitData()
+window.FF_OnAfterSave = function() {
     $.cookie('formCompleted', true, { expire: 365, path: '/'});
-
-    console.log(document.cookie);
-    location = 'resources.html';
-  });
+    location = 'resources.html';  
 }
