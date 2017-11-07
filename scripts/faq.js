453d1f48aa68a676901dbd7d3cd04a7651a2f0a9
werner castanaza <wernercastanaza@gmail.com>
2017-11-07 15:56:40 -0800

    added faq onto the menu as well as the footer

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,9 +1,17 @@
   var g = {};
+  /**
+  * Once the DOM is fully loaded the elements containing the
+  * the class panel-title are fetched and a onClick handler
+  * is set for them.
+  */
 $(document).ready(function() {
-
-
-  g.titles = document.getElementsByClassName("panel-title");
-  $(g.titles).click(function(e) {
+  g.titles = $(".panel-title");
+  $(g.titles).on( "click", toggleClicked);
+});
+  /**
+  * Displays the  answear for that specific question.
+  */
+  function toggleClicked (e) {
     var target = e.target || e.srcElement;
     target = target.parentElement.parentElement;
 
@@ -18,6 +26,4 @@ $(document).ready(function() {
       }
       target.className = "panel-title glyphicon glyphicon-triangle-bottom";
     }
-  });
-
-});
+  }
