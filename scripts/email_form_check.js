a3f0e929b2a9da23b8c5290504857b8facff7728
werner castanaza <wernercastanaza@gmail.com>
2017-11-08 15:54:48 -0800

    bug fix on the email_form_check.js

--- scripts/email_form_check.js
+++ scripts/email_form_check.js
@@ -1,7 +1,13 @@
-var g = {};
+/**
+* Checks if there is a coocki saved and if so this means
+* the user has been redirected to continue filling out the form
+* to obtain the newsLetter. Therefore the email within the coockie
+* is placed in the email input box, this is done so the user dose
+* not have to enter his/her email once again.
+*/
 $(document).ready(function() {
-g.email = document.getElementsByClassName("email_input")[0];
+var email = $("#mce-EMAIL");
 if($.cookie("newslleter_email")){
-  $(g.email).val($.cookie("newslleter_email"));
+  $(email).val($.cookie("newslleter_email"));
 }
 });
